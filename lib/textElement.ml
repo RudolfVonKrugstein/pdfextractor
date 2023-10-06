4522c401bd558db335d755d8fc4a14a1f41696e7
Nathan Hüsken <nathan@wintercloud.de>
2023-10-06 12:05:26 +0200

    [impl] missing pdf text operators

--- lib/textElement.ml
+++ lib/textElement.ml
@@ -23,8 +23,26 @@ let rec from_pdf_ops_rec fc text_state (ops : Pdfops.t list) =
             (TextState.with_font text_state (FontCache.font fc name) size, None)
         | Op_Tr i -> (TextState.with_text_mode text_state i, None)
         | Op_Td (x, y) -> (TextState.translate text_state x y, None)
+        | Op_TD (x, y) ->
+            let text_state = TextState.with_leading text_state (-.y) in
+            (TextState.translate text_state x y, None)
         | Op_T' -> (TextState.newline text_state, None)
         | Op_Tm new_m -> (TextState.with_transform text_state new_m, None)
+        | Op_' s ->
+            (* newline + Tj *)
+            let text_state = TextState.newline text_state in
+            let text, advance =
+              TextState.get_text_and_advance text_state (Pdf.String s)
+            in
+            (TextState.advance text_state advance, Some (text, advance))
+        | Op_'' (ws, cs, s) ->
+            (* newline + Tj *)
+            let text_state = TextState.with_wordspace text_state ws in
+            let text_state = TextState.with_charspace text_state cs in
+            let text, advance =
+              TextState.get_text_and_advance text_state (Pdf.String s)
+            in
+            (TextState.advance text_state advance, Some (text, advance))
         | Op_Tj s ->
             let text, advance =
               TextState.get_text_and_advance text_state (Pdf.String s)
